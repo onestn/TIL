3ccd20b0192f29609e85c516c7e331aad25b9981
onestn <onestone.yang@gmail.com>
2022-06-21 22:56:36 +0900

    feat: custom_test_main

--- my_custom_util/custom_spark/custom_pyspark.py
+++ my_custom_util/custom_spark/custom_pyspark.py
@@ -1,13 +1,11 @@
 import pyspark
+from pyspark.sql.dataframe import DataFrame
 
 from pyspark.sql.functions import col, min, max
 
 def print_min_max(self, col_name: str):
     
-    # TODO: Pyspark 객체 확인 시 Type
-    if type(self) != pyspark.dataframe:
+    if type(self) != DataFrame:
         raise TypeError
     
     print(self.select(min(col(col_name)), max(col(col_name))).show(1, False))
-
-# 
