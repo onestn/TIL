872c777e46a38ceb2eb615e494bbb3f6f159cc3e
onestn <onestone.yang@gmail.com>
2022-07-21 22:04:59 +0900

    chore

--- my_custom_util/custom_spark/custom_datetime.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def get_timetuple(time_obj: datetime):
-    year, month, day, hour, minute, second, *_ = ['{x:02}' for x in time_obj.timetuple()]
-    return year, month, day, hour, minute, second
-
-
-        
